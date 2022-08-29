8cc14ba08b311b4fc079091e6f0e0ad3149e9f3c
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-29 15:37:20 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book 
 
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+
+    def initialize(title)
+        @title = title
+        
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        
+    end
+
+
+end
+
+
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+
+class Shoe
+
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand)
+        @brand = brand
+        
+    end
+
+    def cobble(condition = "new")
+        @condition = condition
+        puts "Your shoe is as good as new!"
+
+    end
+
+
+end
+
+shoe = Shoe.new("Adidas")
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
